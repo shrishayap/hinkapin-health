d5b430a1dc080f433212aaa09478300c3fccd9ee
Shrisha Yapalparvi <shrishayap@gmail.com>
2024-01-16 18:42:20 -0500

    clean up acknowledgment page

--- src/app/acknowledgements/page.tsx
+++ src/app/acknowledgements/page.tsx
@@ -1,30 +1,39 @@
+import Header from "@/components/header";
 
 export function Page() {
     return (
-        <div className='flex flex-col p-2 md:p-4 lg:p-10'>
 
-            <h1 className='text-2xl font-bold'>Acknowledgements</h1>
-            <a href='vecteezy.com' title='Vecteezy'>Hero Image from Vecteezy</a>
-            <a href="https://www.flaticon.com/free-icons/gallbladder" title="gallbladder icons">Gallbladder icons created by Eucalyp - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/stomach" title="stomach icons">Stomach icons created by iconixarPro - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/inguinal-hernia" title="inguinal hernia icons">Inguinal hernia icons created by Freepik - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/kidney-stone" title="kidney stone icons">Kidney stone icons created by Freepik - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/tonsils" title="tonsils icons">Tonsils icons created by kerismaker - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/mother" title="mother icons">Mother icons created by photo3idea_studio - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/medical-doctor" title="medical doctor icons">Medical doctor icons created by Freepik - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/colon" title="colon icons">Colon icons created by surang - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/ent" title="ent icons">Ent icons created by Flat Icons - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/stomach" title="stomach icons">Stomach icons created by Freepik - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/patient" title="patient icons">Patient icons created by Freepik - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/gynecology" title="gynecology icons">Gynecology icons created by Freepik - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/arthritis" title="arthritis icons">Arthritis icons created by Freepik - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/pain" title="pain icons">Pain icons created by popcornarts - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/scalpel" title="scalpel icons">Scalpel icons created by Icon Pond - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/podiatry" title="podiatry icons">Podiatry icons created by Flat Icons - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/spine" title="spine icons">Spine icons created by Freepik - Flaticon</a>
-            <a href="https://www.flaticon.com/free-icons/kidney" title="kidney icons">Kidney icons created by Freepik - Flaticon</a>
-            <a href="https://www.freepik.com/free-vector/hand-drawn-no-data-concept_55024599.htm#query=not%20found&position=29&from_view=search&track=ais&uuid=ebc73210-d351-4e90-929c-28b4fac7240e">Not Found Image by pikisuperstar on Freepik</a> 
-            
+        <div className='flex flex-col space-y-4'>
+
+
+            <Header />
+
+            <div className='flex flex-col space-y-2 px-2 max-w-[1200px] w-full self-center pb-4'>
+                <h1 className='text-2xl font-bold'>Acknowledgements</h1>
+                <a href='vecteezy.com' title='Vecteezy'>Hero Image from Vecteezy</a>
+                <a href="https://www.flaticon.com/free-icons/gallbladder" title="gallbladder icons">Gallbladder icons created by Eucalyp - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/stomach" title="stomach icons">Stomach icons created by iconixarPro - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/inguinal-hernia" title="inguinal hernia icons">Inguinal hernia icons created by Freepik - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/kidney-stone" title="kidney stone icons">Kidney stone icons created by Freepik - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/tonsils" title="tonsils icons">Tonsils icons created by kerismaker - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/mother" title="mother icons">Mother icons created by photo3idea_studio - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/medical-doctor" title="medical doctor icons">Medical doctor icons created by Freepik - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/colon" title="colon icons">Colon icons created by surang - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/ent" title="ent icons">Ent icons created by Flat Icons - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/stomach" title="stomach icons">Stomach icons created by Freepik - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/patient" title="patient icons">Patient icons created by Freepik - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/gynecology" title="gynecology icons">Gynecology icons created by Freepik - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/arthritis" title="arthritis icons">Arthritis icons created by Freepik - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/pain" title="pain icons">Pain icons created by popcornarts - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/scalpel" title="scalpel icons">Scalpel icons created by Icon Pond - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/podiatry" title="podiatry icons">Podiatry icons created by Flat Icons - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/spine" title="spine icons">Spine icons created by Freepik - Flaticon</a>
+                <a href="https://www.flaticon.com/free-icons/kidney" title="kidney icons">Kidney icons created by Freepik - Flaticon</a>
+                <a href="https://www.freepik.com/free-vector/hand-drawn-no-data-concept_55024599.htm#query=not%20found&position=29&from_view=search&track=ais&uuid=ebc73210-d351-4e90-929c-28b4fac7240e">Not Found Image by pikisuperstar on Freepik</a>
+
+            </div>
+
+
         </div>
     )
 }
