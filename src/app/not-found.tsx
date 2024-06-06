ad253d6bc55e0bec7a2bbaba17c8fa8b5fe151ed
Shrisha Yapalparvi <shrishayap@gmail.com>
2024-06-06 12:02:00 -0400

    build error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,7 +11,7 @@ function NotFoundPage() {
 
             <div className='flex flex-col space-y-2 px-2 max-w-[1200px] w-full self-center pb-4 pt-20'>
                 <p className='text-xl font-bold'>404: Page Not Found</p>
-                <p>We're sorry, but the page you're looking for doesn't exist.</p>
+                <p>We&apos;re sorry, but the page you are looking for does not exist.</p>
                 <a href='/' className='hover:underline'>Click here to return to the home page</a>
             </div>
         </div>
